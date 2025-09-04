87fc016c07c9ba59e15b4e39446eb8bd9cf96b61
Jujulis18 <juliethevenin436@gmail.com>
2025-09-04 12:24:05 +0200

    feat: first draft of streamlit interface

---
+++ webapp/modules/page1.py
@@ -0,0 +1,131 @@
+import streamlit as st
+from datetime import datetime
+from PIL import Image
+import time 
+
+if 'show_outfits' not in st.session_state:
+    st.session_state.show_outfits = False
+    st.session_state.outfit_choice = None
+
+def show():
+    st.title("**Welcome to My Mirror on Cloud!**")
+    st.subheader("This application recommends you outfits based on your requests")
+    st.write("Use the tabs above to learn more about the app.")
+
+    #taille des colonnes
+    col1, col2 = st.columns([1, 2], border=True)
+
+    with col1:
+        uploaded_file = st.file_uploader("Upload an image of you today", type=["png", "jpg", "jpeg"])
+        
+        # display the image placeholder (bordered box) define size
+        img = st.empty()
+        img.markdown(
+            """
+            <div style="
+                width:200px;
+                height:200px;
+                border:2px dashed #888;
+                display:flex;
+                align-items:center;
+                justify-content:center;
+                color:#888;
+                border-radius:10px;
+            ">
+                En attente d'image
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
+        
+        
+        if uploaded_file:
+            # Affichage immédiat de l'image
+            image = Image.open(uploaded_file)
+            img.image(image, caption="Image uploadée", width=200)
+
+
+			# --- Section d'analyse ---
+            with st.spinner("Analyse en cours... ⏳"):
+                time.sleep(2)  
+                
+            
+
+    
+    with col2:
+        # Formulaire profil utilisateur
+        st.subheader("Tell us more about you!")
+        with st.form("user_profile_form"):
+            age = st.slider("Age", min_value=0, max_value=120, value=25)
+            gender = st.selectbox("Gender", options=["Male", "Female", "Other"])
+            
+            st.markdown(
+                """
+                <div style="display:flex; align-items:center;">
+                    <h3 style="margin:0;">What are you looking for?</h3>
+                    <span title="Exemple : 
+                    - Casual outfit for a day out with friends
+                    - Formal attire for a wedding
+                    - I'm looking for a summer dress for a beach vacation
+                    - Do you have red top and blue jeans in your collection?" 
+                        style="margin-left:8px; cursor:help; font-size:18px;">ℹ️</span>
+                </div>
+                """,
+                unsafe_allow_html=True
+            )
+        
+                
+            query = st.text_area("Describe your desired outfit or occasion", height=100)
+
+            submitted = st.form_submit_button("Submit")
+            if submitted:
+                st.session_state.show_outfits = True
+                success = st.success("Profile information submitted successfully!", icon="✅")
+
+    if st.session_state.show_outfits :
+        if st.session_state.outfit_choice is None:
+            with st.spinner("Finding the perfect outfit for you... ⏳"):
+                time.sleep(3)  
+                st.balloons()
+                success.empty()
+
+        st.success("Outfit recommendations are ready!")
+        col1, col2, col3 = st.columns([1, 1, 1], border=True)
+        with col1:
+            st.image("https://via.placeholder.com/150", caption="Outfit 1")
+        with col2:
+            st.image("https://via.placeholder.com/150", caption="Outfit 2")
+        with col3:
+            st.image("https://via.placeholder.com/150", caption="Outfit 3")
+
+        # ask choose one outfit
+        st.markdown("### Choose your favorite outfit:")
+        with st.form("choice_form"):
+            outfit_choice = st.radio("Select an outfit", options=["Outfit 1", "Outfit 2", "Outfit 3"])
+            st.session_state.outfit_choice = outfit_choice
+            submitted_choice = st.form_submit_button("Submit")
+            
+            if submitted_choice:
+                
+                st.success(f"You have selected {outfit_choice}!")
+                img.markdown(
+                    """
+                    <div style="
+                        width:200px;
+                        height:200px;
+                        border:2px dashed #888;
+                        display:flex;
+                        align-items:center;
+                        justify-content:center;
+                        color:#888;
+                        border-radius:10px;
+                    ">
+                        Loading image...
+                    </div>
+                    """,
+                    unsafe_allow_html=True
+                )
+                with st.spinner("Switching clothes on your picture... ⏳"):
+                    time.sleep(3)  
+                    img.image("https://via.placeholder.com/200", caption="Your new outfit", width=200)
+
---
+++ webapp/modules/page2.py
@@ -0,0 +1,35 @@
+import streamlit as st
+
+def show():
+    st.title("Learn More About My Mirror on Cloud")
+    st.write("This application recommends you outfits based on your requests")
+
+    # Data sources and technologies used
+    st.markdown("### Data Sources and Technologies Used")
+    st.markdown("""
+    - ...
+    - Streamlit for the web application interface""")
+
+    # Link to dataset used
+    st.markdown("#### Dataset")
+
+
+    # Build the vector database
+    st.markdown("#### Building the Vector Database")
+
+
+    # Recommendation algorithm
+    st.markdown("#### Recommendation Algorithm")
+
+
+    # Swithcing clothes on picture
+    st.markdown("#### Switching Clothes on Picture")
+
+
+    # Future improvements
+    st.markdown("#### Future Improvements")
+
+
+    # Github link
+    st.markdown("#### GitHub Repository")
+    st.markdown("[GitHub Repository](https://github.com/yourusername/yourrepository)")
